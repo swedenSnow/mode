54a8600066ea348374209746540a469282b51259
haz <hani.abuzeid@columbusglobal.com>
2025-07-11 11:04:01 +0200

    chore: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+]
 export default eslintConfig;
